0facedbfd9d6fba4b12f908317ec20eab2ddce74
Gentil-sudo <enzogentil.profissional@gmail.com>
2024-07-06 12:53:26 -0300

    node-api

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 import express from 'express'
 import { PrismaClient } from '@prisma/client'
-
+import cors from 'cors'
 const prisma = new PrismaClient()
 
 const app = express()
 app.use(express.json())
-
+app.use(cors())
 
 app.post('/usuarios', async (req, res) => {
     
@@ -24,7 +24,6 @@ app.post('/usuarios', async (req, res) => {
 app.get('/usuarios',  async (req, res) => {
 
     const users = await prisma.user.findMany()
-
     res.status(200).json(users)
 })
 
